39ff4ba5cbc4d368a4fedc75e84170a28d27e3aa
Shanmugavel29 <vel2982004@gmail.com>
2023-09-12 14:24:38 +0530

    ADD initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import RegistrationFormComponent from "./assets/components/RegistrationFormComponent/RegistrationFormComponent";
+
+function App() {
+  return (
+    <React.Fragment>
+      <RegistrationFormComponent />
+    </React.Fragment>
+  );
+}
+
+export default App;
